69aa9cefb6bf6752d10f70605a762ec812ec847e
Eddie Lee <dudelee@gmail.com>
2012-12-09 22:38:00 +0000

    Bundling and minification of CSS

--- FirstIssue.WebApp/App_Start/BundleConfig.cs
+++ FirstIssue.WebApp/App_Start/BundleConfig.cs
@@ -8,21 +8,12 @@ namespace FirstIssue.WebApp
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-                        
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
+            // CSS bundles
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Styles/normalize.css"));
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Styles/main.css"));
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Styles/normalize.min.css"));
 
+
+            // Enable bundling
             BundleTable.EnableOptimizations = true;
         }
     }
