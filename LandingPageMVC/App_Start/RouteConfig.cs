047505b05a5a06edcf64ce8aee7902519d970dd0
Eddie Lee <dudelee@gmail.com>
2012-12-09 17:59:06 +0000

    Added landing page into the Live branch

---
+++ LandingPageMVC/App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace LandingPageMVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                    "",
+                    "default.aspx",
+                     new { controller = "LandingPage", action = "Index" }
+                );
+
+            routes.MapRoute(
+                name: "LandingPage",
+                url: "{*.url}",
+                defaults: new { controller = "LandingPage", action = "Index" });
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+        }
+    }
+}
