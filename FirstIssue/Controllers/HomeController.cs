f6a532918b9b3a8b16ca829d1528ac1a61ffae1b
Matthew Randle <matt_randle@yahoo.com>
2012-12-09 21:02:13 +0000

    Added T4MVC

--- FirstIssue/Controllers/HomeController.cs
+++ FirstIssue/Controllers/HomeController.cs
@@ -6,9 +6,9 @@ using System.Web.Mvc;
 
 namespace FirstIssue.Controllers
 {
-    public class HomeController : Controller
+    public partial class HomeController : Controller
     {
-        public ActionResult Index()
+        public virtual ActionResult Index()
         {
             return View();
         }
