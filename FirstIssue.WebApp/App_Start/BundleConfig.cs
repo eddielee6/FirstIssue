569f62539729691fd7182e78661c12742493300c
Eddie Lee <dudelee@gmail.com>
2012-12-09 22:06:53 +0000

    Fixed CSS bundle

--- FirstIssue.WebApp/App_Start/BundleConfig.cs
+++ FirstIssue.WebApp/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace FirstIssue.WebApp
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));            
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Styles/main.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Styles/normalize.min.css"));
         }
     }
 }
