b150323533b83d7780283a8f0070e122cd6beb13
Eddie Lee <dudelee@gmail.com>
2012-12-09 20:53:09 +0000

    Removed template code, updated all libraries to latest versions

--- FirstIssue/Controllers/HomeController.cs
+++ FirstIssue/Controllers/HomeController.cs
@@ -10,22 +10,6 @@ namespace FirstIssue.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
