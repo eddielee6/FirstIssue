bcb6898da919003352b22f1ce4d82329beb38307
Weiran Zhang <weiran@zhang.me.uk>
2012-12-10 21:27:37 +0000

    Added skeleton iOS app.

---
+++ FirstIssueApp/FirstIssue/FIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FIAppDelegate.h
+//  FirstIssue
+//
+//  Created by Weiran Zhang on 10/12/2012.
+//  Copyright (c) 2012 First Issue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FirstIssueApp/FirstIssue/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FirstIssue
+//
+//  Created by Weiran Zhang on 10/12/2012.
+//  Copyright (c) 2012 First Issue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FIAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FIAppDelegate class]));
+    }
+}
