949c1b376d6636150c1675c83c5a22393d0c23fc
Eddie Lee <dudelee@gmail.com>
2012-12-10 17:33:39 +0000

    Added twitter bootstrap, setup master pages, setup bundles for js and css

--- FirstIssue.WebApp/App_Start/BundleConfig.cs
+++ FirstIssue.WebApp/App_Start/BundleConfig.cs
@@ -5,13 +5,25 @@ namespace FirstIssue.WebApp
 {
     public class BundleConfig
     {
-        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            // CSS bundles
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Styles/normalize.css"));
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Styles/main.css"));
+            // Libraries
+            var libraryBundle = new ScriptBundle("~/Scripts/Libraries");
+            libraryBundle.Include("~/Scripts/jquery-1.8.3.js");
+            libraryBundle.Include("~/Scripts/modernizr-2.6.2.js");
+            libraryBundle.Include("~/Scripts/bootstrap.js");
+            libraryBundle.Include("~/Scripts/knockout-2.2.0.debug.js");
+            libraryBundle.Include("~/Scripts/jquery.validate.js");
+            libraryBundle.Include("~/Scripts/jquery.validate.unobtrusive.js");
+            libraryBundle.Include("~/Scripts/jquery.unobtrusive-ajax.js");
+            bundles.Add(libraryBundle);
 
+            // CSS
+            var cssBundle = new StyleBundle("~/Styles/CSS");
+            cssBundle.Include("~/Content/Styles/bootstrap.css");
+            cssBundle.Include("~/Content/Styles/bootstrap-responsive.css");
+            cssBundle.Include("~/Content/Styles/main.css");
+            bundles.Add(cssBundle);
 
             // Enable bundling
             BundleTable.EnableOptimizations = true;
