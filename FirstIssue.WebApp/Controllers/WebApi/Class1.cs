96645599c76d5719551e48e4e03f50ed620a45b4
Matthew Randle <matt_randle@yahoo.com>
2012-12-12 19:55:22 +0000

    added webapi folder

---
+++ FirstIssue.WebApp/Controllers/WebApi/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstIssue.WebApp.Controllers.WebApi
+{
+    public class Class1
+    {
+    }
+}
