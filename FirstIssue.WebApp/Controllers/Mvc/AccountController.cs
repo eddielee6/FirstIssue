8952bcabad928c9a72b6e5ea8f6588d03307ab39
Matthew Randle <matt_randle@yahoo.com>
2012-12-12 19:52:05 +0000

    file move

---
+++ FirstIssue.WebApp/Controllers/Mvc/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstIssue.WebApp.Controllers
+{
+    [AllowAnonymous]
+    public partial class AccountController : Controller
+    {        
+        public virtual ActionResult Login()
+        {
+            return View();
+        }
+
+        public virtual ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        public virtual ActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        public virtual ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstIssue.WebApp/Controllers/Mvc/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstIssue.WebApp.Controllers
+{
+    [AllowAnonymous]
+    public partial class HomeController : Controller
+    {
+        public virtual ActionResult Index()
+        {
+            return View();
+        }
+                
+        public virtual ActionResult About()
+        {
+            throw new Exception("500 error page test");
+            return View();
+        }
+
+        public virtual ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
