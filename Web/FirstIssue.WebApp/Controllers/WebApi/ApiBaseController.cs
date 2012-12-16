cc0a3072427869b46c2f5e1510ee762e4749ffb2
Matthew Randle <matt_randle@yahoo.com>
2012-12-16 09:31:20 +0000

    Receipt validation

--- Web/FirstIssue.WebApp/Controllers/WebApi/ApiBaseController.cs
+++ Web/FirstIssue.WebApp/Controllers/WebApi/ApiBaseController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using FirstIssue.WebApp.Models;
 
 namespace FirstIssue.WebApp.Controllers
 {
-    public class ApiBaseController
+    public class ApiBaseController : ApiController
     {
         protected FirstIssueContext _dbContext { get; private set; }
 
---
+++ Web/FirstIssue.WebApp/Controllers/WebApi/ReceiptVerificationResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstIssue.WebApp.Controllers.WebApi
+{
+    public class ReceiptVerificationResult
+    {
+        public bool IsReceiptValid { get; set; }
+    }
+}
